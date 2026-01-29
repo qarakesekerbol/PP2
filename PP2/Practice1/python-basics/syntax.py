7fc23216d71623a607de98b8268643a497bbd4dd
ZheksembaevErbol <e_zheksembaev@kbtu.kz>
2026-01-29 16:12:48 +0500

    prac01

---
+++ PP2/Practice1/python-basics/syntax.py
@@ -0,0 +1,18 @@
+#1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#3
+x = 5
+y = "Hello, World!"
+
+#4
+#This is a comment.
+print("Hello, World!")
+
