48a64b6ab2cc28f5f1af0bc7b8732addc21c5823
ZheksembaevErbol <e_zheksembaev@kbtu.kz>
2026-02-05 23:38:19 +0500

    prac02

---
+++ Practice2/boolean/Booleans as Comparison Results.py
@@ -0,0 +1,28 @@
+#1
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+#2
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+
+#3
+x = 5
+
+print(x > 0 and x < 10)
+
+#4
+x = 5
+
+print(not(x > 3 and x < 10))
