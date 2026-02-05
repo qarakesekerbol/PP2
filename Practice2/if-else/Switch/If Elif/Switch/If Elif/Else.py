881887045c76fb42d67b1d6527fba2bfec45aa72
ZheksembaevErbol <e_zheksembaev@kbtu.kz>
2026-02-06 00:21:47 +0500

    prac02

---
+++ Practice2/if-else/Switch/If Elif/Switch/If Elif/Else.py
@@ -0,0 +1,47 @@
+#1
+value = 50
+
+if value < 0:
+  print("Negative value")
+elif value == 0:
+  pass # Zero case - no action needed
+else:
+  print("Positive value")
+
+#2
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
+#3
+age = 25
+has_license = True
+
+if age >= 18:
+  if has_license:
+    print("You can drive")
+  else:
+    print("You need a license")
+else:
+  print("You are too young to drive")
+
+#4
+score = 85
+attendance = 90
+submitted = True
+
+if score >= 60:
+  if attendance >= 80:
+    if submitted:
+      print("Pass with good standing")
+    else:
+      print("Pass but missing assignment")
+  else:
+    print("Pass but low attendance")
+else:
+  print("Fail")
