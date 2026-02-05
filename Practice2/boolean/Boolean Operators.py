f103e7774d73a19c5737448fa5aafaa401fa9e84
ZheksembaevErbol <e_zheksembaev@kbtu.kz>
2026-02-05 23:40:45 +0500

    prac02

---
+++ Practice2/boolean/Boolean Operators.py
@@ -0,0 +1,30 @@
+#1
+sum1 = 100 + 50      # 150 (100 + 50)
+sum2 = sum1 + 250    # 400 (150 + 250)
+sum3 = sum2 + sum2   # 800 (400 + 400)
+
+#2
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#3
+x = 12
+y = 5
+
+print(x / y)
+
+#4
+x = 12
+y = 5
+
+print(x // y)
+
+
