df6853aa3b1f8fc2590801f36da095472b5342ef
ZheksembaevErbol <e_zheksembaev@kbtu.kz>
2026-02-19 20:58:50 +0500

    prac03

---
+++ Practice3/functions/basic_functions.py
@@ -0,0 +1,17 @@
+#1
+def my_function():
+  print("Hello from a function")
+
+#2
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+#3
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
